5b1f3ba06b71682238bc77316f97d4f8071c3156
MerezkaYurii <shkolasad@ukr.net>
2024-11-03 10:20:16 +0200

    var2

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,9 @@ const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", handlerInput);
 function handlerInput(event){
-    if(textInput.value === " " || "") {
-    textInput.value = "Anonymous"} 
-         textOutput.innerHTML=textInput.value.trim();
+    if(textInput.value === " ") {
+    textInput.value = "Anonymous"};
+    textOutput.innerHTML=textInput.value.trim();
     return;
 }
 
